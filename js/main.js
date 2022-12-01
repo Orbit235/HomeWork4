ee83bce5536ebd6db5042b646b68eca90b01a673
Orbit235 <v.ionichev@gmail.com>
2022-12-01 20:12:59 +0300

    feat: validation added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$('#form>first-form).click((e) => {
-    e.preventDefault()
-    let data = $('#first-form').serializeArray()
+$( "#first-form" ).click(function( event ) {
+    event.preventDefault();
+      });
+
+  jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid"
+  });  
+        rules( "#first-form", {
+            rules: {
+                x: {
+                  required: true,
+                  range: [-4, 4]
+                }
+              }
+            });
+    
