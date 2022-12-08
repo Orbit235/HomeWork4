b0acfdfc3c4bc1478e2d4bc9e5ac4aa7a5dc676e
Orbit235 <v.ionichev@gmail.com>
2022-12-08 22:09:48 +0300

    Y result added, fields result added, error field Y added

--- js/main.js
+++ js/main.js
@@ -14,13 +14,23 @@ validateY.max = 5
 const validateR = (r) => {
     return r != "" && (Number(r) > validateR.min && Number(r) <= validateR.max)
 }
-validateR.min = 0
+validateR.min = -1
 validateR.max = 4
 
 // x, y, r - уже числа
 const validateRectangle = (x, y, r) => {
     return (x >= 0 && x <= r / 2) && (0 <= y && y <= r)
 }
+// Я добавил стрелочную функцию, которая устанавливает область попадания для нижней правой части рисунка↓
+const validateRectangle1 = (x, y, r) => {
+    return (x >= 0 && x <= r) && (-r / 2 <= y && y <= 0)
+}
+
+// Я добавил стрелочную функцию, которая устанавливает область попадания для нижней левой части рисунка↓
+const validateRectangle2 = (x, y, r) => {
+    return (x >= -r / 2 && x <= 0) && (-r / 2 <= y && y <= 0)
+}
+
 
 $(function() {
 
@@ -42,18 +52,23 @@ $(function() {
 
         if (validateY(y)) {
             console.log("Y верный")
+            $(".errorY").html("")
         } else {
             console.log("Y не верный");
+            // Добавил поле ошибки для Y
+            $(".errorY").html("Y не входит в диапазон (-3; 5)")
         }
 
         if (validateR(r)) {
-            console.log("R верный")
+            console.log("R верный")            
         } else {
             console.log("R не верный");
         }
 
         // Конечный результат
         const validateResult = validateR(r) && validateY(y) && validateX(x)
+        
+        
 
         if (validateResult) {
             const hitResult = validateRectangle(Number(x), Number(y), Number(r))
@@ -70,8 +85,45 @@ $(function() {
             `)
         }
 
+          // Я добавил функцию, которая описывает факт попадания в область для нижней правой части рисунка↓
+          const validateResult1 = validateR(r) && validateY(y) && validateX(x)
+        if (validateResult1) {
+            const hitResult1 = validateRectangle1(Number(x), Number(y), Number(r))
+
+            console.log(values);
+
+            $(".result-table1").append(`
+            <tr>
+                <th>${x}</th>
+                <th>${y}</th>
+                <th>${r}</th>
+                <th>${hitResult1}</th>
+            </tr>
+            `)
+        }
+        // Я добавил функцию, которая описывает факт попадания в область для нижней левой части рисунка↓
+        const validateResult2 = validateR(r) && validateY(y) && validateX(x)
+        if (validateResult2) {
+            const hitResult2 = validateRectangle2(Number(x), Number(y), Number(r))
+
+            console.log(values);
+
+            $(".result-table2").append(`
+            <tr>
+                <th>${x}</th>
+                <th>${y}</th>
+                <th>${r}</th>
+                <th>${hitResult2}</th>
+            </tr>
+            `)
+        }
+
 
     })
 
 
 });
+
+
+
+
